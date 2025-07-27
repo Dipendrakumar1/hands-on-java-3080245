015ad42513fc68e123e683117014543044f0ea77
Dipendra yadav <76865286+Dipendrakumar1@users.noreply.github.com>
2025-07-27 15:43:20 +0000

    Added getCutomer method to get customer with their user id.

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -2,9 +2,12 @@ package bank;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DataSource {
+  // Method to create connection with database.
   public static Connection connect() {
     String dbFile = "jdbc:sqlite:resources/bank.db";
     Connection connection = null;
@@ -17,18 +20,54 @@ public class DataSource {
     return connection;
   }
 
-  public static Customer getCustomer(String userName){
-     Customer cus=null;
-     return cus;
+  // Method to fetch the records of given user
+  public static Customer getCustomer(String userName) {
+    Customer cus = null;
+    // This is preparedStatement
+    String query = "select * from customers where username=?";
+    try {
+      Connection conn = connect();
+      PreparedStatement pState = conn.prepareStatement(query);
+      pState.setString(1, userName);
+      ResultSet resultSet = pState.executeQuery();
+      boolean flag = false;
+      while (resultSet.next()) {
+        flag = true;
+        int id = resultSet.getInt("id");
+        String name = resultSet.getString("name");
+        String username = resultSet.getString("username");
+        String password = resultSet.getString("password");
+        int accountId = resultSet.getInt("account_id");
+        cus = new Customer(id, name, username, password, accountId);
+      }
+      if (!flag) {
+        System.out.println("Records Not Found.");
+      }
+      try {
+        conn.close();
+        System.out.println("Connection Closed Successfully.");
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    return cus;
   }
 
   public static void main(String[] args) {
-    Connection cc = connect();
-    try {
-      cc.close();
-      System.out.println("Database closed.");
-    } catch (SQLException e) {
-      e.printStackTrace();
+    // Connection cc = connect();
+    // try {
+    // cc.close();
+    // System.out.println("Database closed.");
+    // } catch (SQLException e) {
+    // e.printStackTrace();
+    // }
+    Customer cus = getCustomer("ttoulchi5@ehow.com");
+    if (cus != null) {
+      System.out.println(cus.getId() + "\n" + cus.getName() + "\n" + cus.getPassword());
     }
 
   }
